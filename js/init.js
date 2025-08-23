dffec7f2f3a00ffa118ed38232b292c2ae0f282c
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-23 15:32:06 -0500

    Add the main menu

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
 import {create_loading_screen, load_assets} from './loading.js'
-// import {create_main_menu} from './main-menu.js'
+import {create_main_menu} from './main-menu.js'
 import {create_error_screen} from './error.js'
 
 async function initialize_csm() {
     create_loading_screen()
     await load_assets()
-    // create_main_menu()
+    create_main_menu()
 }
 
 window.addEventListener('load', initialize_csm)
