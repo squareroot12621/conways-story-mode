5ae624fff32017b3c06b52f21e2e2a2f3a249c1c
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-23 09:13:18 -0500

    Add glider_canvas

--- js/loading.js
+++ js/loading.js
@@ -11,8 +11,17 @@ function create_loading_screen() {
     var loading_container = create_element(
         'div', [loading_text, progress_bar], {class: 'loading-container'}
     )
-    loading_container.style.setProperty('--loading-percentage', 0.42)
-    update_root(loading_container)
+    loading_container.style.setProperty('--loading-percentage', 0)
+
+    var root = document.getElementById('conways-story-mode')
+    var root_size = Math.min(root.clientWidth, root.clientHeight)
+    var canvas_size = Math.round(root_size / 2) + 'px'
+    var glider_canvas = create_element('canvas', [], {
+        id: 'loading-canvas',
+        width: canvas_size,
+        height: canvas_size,
+    })
+    update_root(loading_container, glider_canvas)
 }
 
 async function load_assets() {
