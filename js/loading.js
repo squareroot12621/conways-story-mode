787335271dc4b2f3761eb83421a4cc4309b5d3ab
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-23 09:40:30 -0500

    Make the canvas not just sit there all day

--- js/loading.js
+++ js/loading.js
@@ -20,8 +20,44 @@ function create_loading_screen() {
         id: 'loading-canvas',
         width: canvas_size,
         height: canvas_size,
+        'data-frame': 0,
     })
+
+    update_glider_canvas()
     update_root(loading_container, glider_canvas)
+    setInterval(update_glider_canvas, 55) // 18 FPS = 55 ms
+}
+
+function update_glider_canvas() {
+    var glider_phases = [
+        [[1, 0], [2, 1], [0, 2], [1, 2], [2, 2]],
+        [[0, 1], [2, 1], [1, 2], [2, 2], [1, 3]],
+        [[2, 1], [0, 2], [2, 2], [1, 3], [2, 3]],
+        [[1, 1], [2, 2], [3, 2], [1, 3], [2, 3]],
+    ]
+    var cell_size = 6
+    var grid_buffer = 10
+    const glider_canvas = document.getElementById('loading-canvas')
+    const ctx = glider_canvas.getContext('2d')
+    var grid_size = Math.ceiling(glider_canvas.clientWidth / cell_size)
+    var current_frame = glider_canvas.getAttribute('data-frame')
+    
+    var glider_x = -grid_buffer
+    var glider_y = -grid_buffer
+    var glider_frame = current_frame
+    ctx.fillStyle = '#E3E5E5'
+    while (glider_x < grid_size + grid_buffer && glider_y < grid_size + grid_buffer) {
+        glider_x = Math.floor(glider_frame / 4) - grid_buffer
+        glider_y = Math.floor(glider_frame / 4) - grid_buffer
+        for (cell of glider_phases[glider_frame % 4]) {
+            cell_x = cell[0] + glider_x
+            cell_y = cell[1] + glider_y
+            ctx.fillRect(cell_x * cell_size, cell_y * cell_size, cell_size, cell_size)
+        }
+        glider_frame += 30
+    }
+
+    glider_canvas.setAttribute('data-frame', (current_frame + 1) % 30)
 }
 
 async function load_assets() {
