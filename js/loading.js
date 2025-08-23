10880b138a5fa1b38d3598b1ff544c64f7eb7863
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-23 07:19:38 -0500

    Change structure of loading screen

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,14 @@
 import {create_element, update_root} from './utilities.js'
 
 function create_loading_screen() {
-    var loading_title = create_element('p', 'Loading\u2026')
     var loading_progress = create_element(
-        'p', '(0%)', {id: 'loading-progress', 'aria-busy': 'true'}
+        'span', '0%', {id: 'loading-progress', 'aria-busy': 'true'}
     )
-    var loading_container = create_element('div', [loading_title, loading_progress])
+    var loading_text = create_element(
+        'p', ['Loading\u2026', loading_progress], {class: 'loading-text'}
+    )
+    var progress_bar = create_element('div', [], {class: 'loading-bar'})
+    var loading_container = create_element('div', [loading_text, progress_bar])
     update_root(loading_container)
 }
 
