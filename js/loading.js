613efd87c69b4379f1301448d54c2ca8ba817752
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-23 08:36:50 -0500

    Test --loading-progress being other numbers

--- js/loading.js
+++ js/loading.js
@@ -2,7 +2,7 @@ import {create_element, update_root} from './utilities.js'
 
 function create_loading_screen() {
     var loading_progress = create_element(
-        'span', '0%', {id: 'loading-progress', 'aria-busy': 'true'}
+        'span', '42%', {id: 'loading-progress', 'aria-busy': 'true'}
     )
     var loading_text = create_element(
         'p', ['Loading\u2026 ', loading_progress], {class: 'loading-text'}
@@ -11,7 +11,7 @@ function create_loading_screen() {
     var loading_container = create_element(
         'div', [loading_text, progress_bar], {class: 'loading-container'}
     )
-    loading_container.style.setProperty('--loading-progress', 0)
+    loading_container.style.setProperty('--loading-progress', 42)
     update_root(loading_container)
 }
 
