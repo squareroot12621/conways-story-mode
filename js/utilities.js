ef327fb90bb267dfecad0ff12db37871a37cd0d8
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-23 21:44:19 -0500

    Calculate a better number for heading widths

--- js/utilities.js
+++ js/utilities.js
@@ -14,17 +14,12 @@ function create_element(tag, content=[], attributes={}) {
 function update_root(...elements) {
     var root = document.getElementById('conways-story-mode')
     root.replaceChildren(...elements)
-
-    var TEST = document.createElement('p')
-    TEST.append('Test')
-    root.append(TEST)
-    
-    var heading_width = '75' // TODO: Change to something more dynamic
+    var width = document.getElementById('body-wrapper').clientWidth
+    var heading_width = Math.min(75 + 25 * width/320, 100) + '%'
     var all_headings = root.querySelectorAll('h1, h2, h3, h4, h5, h6')
-    console.log(all_headings)
     for (var heading of all_headings) {
-        heading.style.setProperty('font-stretch', heading_width + '%')
-        heading.style.setProperty('font-width', heading_width + '%')
+        heading.style.setProperty('font-stretch', heading_width)
+        heading.style.setProperty('font-width', heading_width)
     }
 }
 
