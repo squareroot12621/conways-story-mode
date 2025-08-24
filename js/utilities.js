3c7018e85efb493213a953141677b99679a117b2
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-23 21:25:35 -0500

    Show all_headings

--- js/utilities.js
+++ js/utilities.js
@@ -15,7 +15,9 @@ function update_root(...elements) {
     var root = document.getElementById('conways-story-mode')
     root.replaceChildren(...elements)
     var heading_width = '75' // TODO: Change to something more dynamic
-    for (var heading of root.querySelectorAll('h1, h2, h3, h4, h5, h6')) {
+    var all_headings = root.querySelectorAll('h1, h2, h3, h4, h5, h6')
+    console.log(all_headings)
+    for (var heading of all_headings) {
         heading.style.setProperty('font-stretch', heading_width + '%')
         heading.style.setProperty('font-width', heading_width + '%')
     }
