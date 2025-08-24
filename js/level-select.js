b2c1456fdb1fe399d2e9d84a8c81822c5f760c38
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-24 13:18:19 -0500

    Create level-select.js

---
+++ js/level-select.js
@@ -0,0 +1,25 @@
+import {create_element, update_root} from './utilities.js'
+
+function create_level_select() {
+    var back_icon = create_element('span', 'arrow_back', {class: 'icon', alt: ''})
+    var back_button = create_element('button', [back_icon, ' Back'], {class: 'back-button'})
+
+    var units_heading = create_element('h3', 'Units', {class: 'levels-heading'})
+    var unit_array = []
+    for (var i = 1; i <= 10; ++i) {
+        unit_array.push(create_element('li', i.toString()))
+    }
+    var units = create_element('ol', unit_array, {class: 'levels-units'})
+    
+    var lessons_heading = create_element('h3', 'Lessons', {class: 'levels-heading'})
+    var lesson_array = []
+    for (var i = 1; i <= 10; ++i) {
+        lesson_array.push(create_element('li', i.toString()))
+    }
+    var lessons = create_element('ol', lesson_array, {class: 'levels-lessons'})
+    var lessons_wrapper = create_element('div', lessons, {class: 'levels-lessons-wrapper'})
+
+    update_root(back_button, units_heading, units, lessons_heading, lessons_wrapper)
+}
+
+export {create_level_select}
