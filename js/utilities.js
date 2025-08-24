a05e4ad2fd85c580f407c7ca8a07e5fd646ca07c
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-23 21:24:38 -0500

    Make all headings 75% width for right now

--- js/utilities.js
+++ js/utilities.js
@@ -1,19 +1,24 @@
 function create_element(tag, content=[], attributes={}) {
-  var element = document.createElement(tag)
-  if (content instanceof Array) {
-    element.append(...content)
-  } else {
-    element.append(content)
-  }
-  for (var [key, value] of Object.entries(attributes)) {
-    element.setAttribute(key, value)
-  }
-
-  return element
+    var element = document.createElement(tag)
+    if (content instanceof Array) {
+        element.append(...content)
+    } else {
+        element.append(content)
+    }
+    for (var [key, value] of Object.entries(attributes)) {
+        element.setAttribute(key, value)
+    }
+    return element
 }
 
 function update_root(...elements) {
-  document.getElementById('conways-story-mode').replaceChildren(...elements)
+    var root = document.getElementById('conways-story-mode')
+    root.replaceChildren(...elements)
+    var heading_width = '75' // TODO: Change to something more dynamic
+    for (var heading of root.querySelectorAll('h1, h2, h3, h4, h5, h6')) {
+        heading.style.setProperty('font-stretch', heading_width + '%')
+        heading.style.setProperty('font-width', heading_width + '%')
+    }
 }
 
 export {create_element, update_root}
