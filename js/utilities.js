ce00b8379c4e193be7549dad8018c7f5085c6382
squareroot12621 <mathandcoding2718@gmail.com>
2025-08-23 21:47:34 -0500

    Fix width equation and add a debug element

--- js/utilities.js
+++ js/utilities.js
@@ -15,7 +15,8 @@ function update_root(...elements) {
     var root = document.getElementById('conways-story-mode')
     root.replaceChildren(...elements)
     var width = document.getElementById('body-wrapper').clientWidth
-    var heading_width = Math.min(75 + 25 * width/320, 100) + '%'
+    var heading_width = Math.min(75 + 25 * (width-320)/320, 100) + '%'
+    root.append(width + '')
     var all_headings = root.querySelectorAll('h1, h2, h3, h4, h5, h6')
     for (var heading of all_headings) {
         heading.style.setProperty('font-stretch', heading_width)
