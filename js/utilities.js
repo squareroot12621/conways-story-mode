0a7b84f18feb0c4afab418f9e0fa5db3ee8030c4
squareroot12621 <mathandcoding2718@gmail.com>
2025-09-06 22:24:34 -0500

    Add throttle

--- js/utilities.js
+++ js/utilities.js
@@ -54,4 +54,40 @@ function resize_root() {
     }
 }
 
-export {create_element, update_root, resize_root}
+// Adapted from https://stackoverflow.com/a/27078401
+// Simplified to always fire on the leading and trailing edges
+function throttle(func, wait) {
+  var context, args, result
+  var timeout = null
+  var previous = 0
+  var later = function() {
+    previous = Date.now()
+    timeout = null
+    result = func.apply(context, args)
+    if (!timeout) {
+      context = args = null
+    }
+  }
+  return function() {
+    var now = Date.now()
+    var remaining = wait - (now - previous)
+    context = this
+    args = arguments
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+      }
+      previous = now
+      result = func.apply(context, args)
+      if (!timeout) {
+        context = args = null
+      }
+    } else if (!timeout) {
+      timeout = setTimeout(later, remaining)
+    }
+    return result
+  }
+}
+
+export {create_element, update_root, resize_root, throttle}
