1082ab4a6401c536893e783013f5df5c04d28936
squareroot12621 <mathandcoding2718@gmail.com>
2025-09-06 22:25:32 -0500

    Throttle resize_root

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 import {create_loading_screen, load_assets} from './loading.js'
 import {create_main_menu} from './main-menu.js'
 import {create_error_screen} from './error.js'
-import {resize_root} from './utilities.js'
+import {resize_root, throttle} from './utilities.js'
 
 async function initialize_csm() {
     create_loading_screen()
@@ -13,5 +13,6 @@ window.addEventListener('load', initialize_csm)
 window.addEventListener('error', create_error_screen)
 window.addEventListener('unhandledrejection', create_error_screen)
 // window.addEventListener('resize') is too unreliable
-const resize_observer = new ResizeObserver(resize_root)
+var resize_root_throttled = throttle(resize_root, 100)
+const resize_observer = new ResizeObserver(resize_root_throttled)
 resize_observer.observe(document.getElementById('conways-story-mode'))
