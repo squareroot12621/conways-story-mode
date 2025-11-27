314b6a65b5b3b4e9669b1b375db7d447c0cb5716
squareroot12621 <mathandcoding2718@gmail.com>
2025-11-27 09:39:48 -0600

    This is confusing.

--- js/cgol.js
+++ js/cgol.js
@@ -444,6 +444,7 @@ class CGoL {
        This can happen if you undo something and then do an action normally. */
     if (this.#current_undo_state < this.#undo_snapshots.length - 1) {
       this.#undo_snapshots.splice(this.#current_undo_state + 1)
+      console.log('branch deleted') // DEBUG
     }
     // Make the state and push it into the snapshot array.
     this.#undo_snapshots.push({
@@ -475,7 +476,6 @@ class CGoL {
       // Remove previous_state.
       this.#undo_snapshots.splice(this.#undo_snapshots.length - 2, 1)
       --this.#current_undo_state
-      console.log('merged') // DEBUG
       if (new_state.value1 === 0 && new_state.value2 === 0
           // Edge case if this.#max_undo_snapshots is really low
           && this.#undo_snapshots.length > 1) {
