1bd56cb54d805bf2e54805fbe8bf97bd143e75ab
squareroot12621 <mathandcoding2718@gmail.com>
2026-01-02 13:31:54 -0600

    Why is it saying it's readonly?

--- js/loading.js
+++ js/loading.js
@@ -125,6 +125,11 @@ async function load_assets() {
 
     // Load the library object JSON
     var library_object_response = await fetch('https://cdn.jsdelivr.net/gh/squareroot12621/conways-story-mode@a3132b3/data/library-objects.json')
+    // START DEBUG
+    console.log(object_data)
+    object_data['test'] = 'test'
+    console.log(object_data)
+    // END DEBUG
     object_data = await library_object_response.json()
     ++tasks_done
     update_progress(tasks_done / tasks_to_do)
