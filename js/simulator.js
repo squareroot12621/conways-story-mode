a88a6653cb2323f9e7181a501ce060156a1056a0
squareroot12621 <mathandcoding2718@gmail.com>
2026-01-03 11:46:25 -0600

    The space between the "add object" button and the text disappears when clicking the button

--- js/simulator.js
+++ js/simulator.js
@@ -735,7 +735,7 @@ function create_event_handlers(sandbox) {
         // Edit the text next to the button
         // TODO: Add support for other languages
         var data_name = current_object_data.name['en-US']
-        var object_info = `${data_count}\u00D7 ${data_name}`
+        var object_info = `${data_count}\u00D7 ${data_name} `
         add_object_button.previousSibling.data = object_info
       }
       if (data_count <= 0) {
